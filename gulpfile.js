61098c9ec2c799342c3aff33cd7cc4939b7a9d2b
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2019-02-21 20:24:14 +0100

    Switched to Gulp 4 + added purgecss

--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,108 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var autoprefixer = require('gulp-autoprefixer');
-var pkg = require('./package.json');
-var concat = require("gulp-concat");
-var imageResize = require('gulp-image-resize');
-var concatCss = require('gulp-concat-css');
+const {series, parallel, task, src, dest, watch} = require('gulp');
+const sass = require('gulp-sass');
+const header = require('gulp-header');
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const pkg = require('./package.json');
+const concat = require("gulp-concat");
+const imageResize = require('gulp-image-resize');
+const concatCss = require('gulp-concat-css');
+const plumber = require('gulp-plumber');
+const image = require('gulp-image');
+const newer = require('gulp-newer');
+const purgecss = require('gulp-purgecss');
+const merge = require('merge-stream');
 
 // Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>',
-    ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)',
+const banner = ['/*!',
+    ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)',
+    ' * Copyright 2013 - ' + (new Date()).getFullYear(),
+    ' * Licensed under <%= pkg.license %> (https://github.com/guillaume-sainthillier/<%= pkg.name %>/blob/master/LICENSE)',
     ' */',
     ''
 ].join('\n');
 
 // Copy vendor to public path
-gulp.task('vendor:group', function () {
+function vendorGroup() {
     // Bootstrap
-    gulp.src([
+    var bootstrap = src([
         './node_modules/bootstrap/dist/**/*',
         '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
         '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ]).pipe(gulp.dest('./public/vendor/bootstrap'));
+    ]).pipe(dest('./public/vendor/bootstrap'));
 
     // Font Awesome
-    gulp.src([
+    var fa = src([
         './node_modules/@fortawesome/**/*',
-    ]).pipe(gulp.dest('./public/vendor'));
+    ]).pipe(dest('./public/vendor'));
 
     // jQuery
-    gulp.src([
+    var jquery = src([
         './node_modules/jquery/dist/*',
         '!./node_modules/jquery/dist/core.js'
-    ]).pipe(gulp.dest('./public/vendor/jquery'));
+    ]).pipe(dest('./public/vendor/jquery'));
 
     // jQuery Easing
-    gulp.src([
+    var jqueryEasing = src([
         './node_modules/jquery.easing/*.js'
-    ]).pipe(gulp.dest('./public/vendor/jquery-easing'));
+    ]).pipe(dest('./public/vendor/jquery-easing'));
 
     // jqCloud
-    gulp.src([
+    var jqCloud = src([
         './node_modules/jqcloud2/dist/*'
-    ]).pipe(gulp.dest('./public/vendor/jqcloud2'));
+    ]).pipe(dest('./public/vendor/jqcloud2'));
 
     //jqBootstrapValidation Custom lib
-    gulp.src([
+    var jqBootstrapValidation = src([
         './assets/vendor/jqBootstrapValidation.js'
-    ]).pipe(gulp.dest('./public/vendor/jqBootstrapValidation'));
-});
+    ]).pipe(dest('./public/vendor/jqBootstrapValidation'));
 
+    return merge(bootstrap, fa, jquery, jqueryEasing, jqCloud, jqBootstrapValidation);
+}
 
 //Concat and minify CSS AND JS
-gulp.task('vendor:concat', function () {
-    gulp.src([
-        'public/vendor/jquery/jquery.js',
-        'public/vendor/jquery-easing/jquery.easing.js',
-        'public/vendor/bootstrap/js/bootstrap.bundle.js',
-        'public/vendor/jqcloud2/jqcloud.js',
-        'public/vendor/jqBootstrapValidation/jqBootstrapValidation.js',
-    ])
-        .pipe(concat('bundle.js'))
-        .pipe(gulp.dest("public/vendor"))
-        .pipe(rename('bundle.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest("public/vendor"))
-        ;
+function vendorConcat() {
+    var js = src([
+            'public/vendor/jquery/jquery.js',
+            'public/vendor/jquery-easing/jquery.easing.js',
+            'public/vendor/bootstrap/js/bootstrap.bundle.js',
+            'public/vendor/jqcloud2/jqcloud.js',
+            'public/vendor/jqBootstrapValidation/jqBootstrapValidation.js',
+        ])
+            .pipe(concat('bundle.js'))
+            .pipe(dest("public/vendor"))
+            .pipe(rename('bundle.min.js'))
+            .pipe(uglify())
+            .pipe(dest("public/vendor"))
+    ;
 
-    gulp.src([
+    var css = src([
         'public/css/bootstrap.css',
         'public/vendor/fontawesome-free/css/all.css',
         'public/vendor/jqcloud2/jqcloud.css',
     ])
         .pipe(concatCss("bundle.css"))
-        .pipe(gulp.dest('public/vendor'))
+        .pipe(dest('public/vendor'))
         .pipe(cleanCSS())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('public/vendor'));
-});
+        .pipe(dest('public/vendor'));
 
-gulp.task('vendor', ['vendor:group', 'vendor:concat']);
+    return merge(css, js);
+}
 
 // Compile SCSS to CSS
-gulp.task('css:compile', function () {
-    return gulp.src('./assets/scss/**/*.scss')
-        .pipe(sass.sync({
-            includePaths: [
-                'node_modules'
-            ],
-            outputStyle: 'expanded'
-        }).on('error', sass.logError))
+function cssCompile() {
+    return src('./assets/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass({
+            outputStyle: "expanded",
+            includePaths: "./node_modules",
+        }))
+        .on("error", sass.logError)
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
@@ -104,12 +110,26 @@ gulp.task('css:compile', function () {
         .pipe(header(banner, {
             pkg: pkg
         }))
-        .pipe(gulp.dest('./public/css'))
-});
+        .pipe(
+            purgecss({
+                content: ['./public/*.php', 'required/*.php', './assets/js/*.js', './node_modules/jqcloud2/dist/*.js'],
+                whitelist: [
+                    'show', 'hide', 'showing',
+                    'fade', 'active', 'focus','disabled',
+                    'btn',
+                    'collapse', 'collapsing', 'collapsed',
+                    'dropdown-menu', 'dropdown-menu-right', 'dropdown-menu-left', 'position-static', 'dropleft', 'dropright', 'dropup', 'dropdown-item', 'dropdown-menu',
+                    'modal-open', 'modal-backdrop', 'modal-scrollbar-measure', 'modal-dialog-scrollable'
+                ],
+                whitelistPatterns: [/^w(\d+)$/],
+            })
+        )
+        .pipe(dest('./public/css'))
+}
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function () {
-    return gulp.src([
+function cssMinify() {
+    return src([
         './public/css/*.css',
         '!./public/css/*.min.css'
     ])
@@ -117,23 +137,21 @@ gulp.task('css:minify', ['css:compile'], function () {
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./public/css'));
-});
-
-gulp.task('css', ['css:compile', 'css:minify']);
+        .pipe(dest('./public/css'));
+}
 
 // Concat JS
-gulp.task('js:concat', function () {
-    return gulp.src([
+function jsConcat() {
+    return src([
         './assets/js/*.js',
     ])
         .pipe(concat('agency.js'))
-        .pipe(gulp.dest("public/js"));
-});
+        .pipe(dest("public/js"));
+}
 
 // Minify JavaScript
-gulp.task('js:minify', function () {
-    return gulp.src([
+function jsMinify() {
+    return src([
         './public/js/*.js',
         '!./public/js/*.min.js',
     ])
@@ -144,102 +162,128 @@ gulp.task('js:minify', function () {
         .pipe(header(banner, {
             pkg: pkg
         }))
-        .pipe(gulp.dest('./public/js'));
-});
+        .pipe(dest('./public/js'));
+}
 
-gulp.task('js', ['js:concat', 'js:minify']);
 
-gulp.task('img:copy', function () {
-    return gulp.src([
-        './assets/img/*.*',
-    ])
-        .pipe(gulp.dest('./public/img'))
-});
-
-gulp.task('img:resize', function () {
+function img() {
     //Portfolio resize
-    gulp.src('./assets/img/realisations/*.jpg')
+    var realisations = src('./assets/img/realisations/*.jpg')
+        .pipe(newer('./public/img/realisations'))
         .pipe(imageResize({
-            width : 700,
-            height: 700 * 4/3,
+            width: 700,
+            height: 700 * 4 / 3,
             imageMagick: true, //PNG to JPG is better here
             noProfile: true
         }))
-        .pipe(gulp.dest('./public/img/realisations'));
+        .pipe(image())
+        .pipe(dest('./public/img/realisations'));
 
-    gulp.src('./assets/img/realisations/*.jpg')
+    var realisationsRetina = src('./assets/img/realisations/*.jpg')
+        .pipe(newer('./public/img/realisations'))
         .pipe(imageResize({
-            width : 1400,
-            height: 1400 * 4/3,
+            width: 1400,
+            height: 1400 * 4 / 3,
             imageMagick: true, //PNG to JPG is better here
             noProfile: true
         }))
-        .pipe(rename(function (path) { path.basename += "@2x"; }))
-        .pipe(gulp.dest('./public/img/realisations'));
+        .pipe(image())
+        .pipe(rename(function (path) {
+            path.basename += "@2x";
+        }))
+        .pipe(dest('./public/img/realisations'));
+
 
     //Logos resize
-    gulp.src('./assets/img/logos/*')
+    var logos = src('./assets/img/logos/*.jpg')
+        .pipe(newer('./public/img/logos'))
         .pipe(imageResize({
-            width : 200,
-            height : 200,
-            crop : true,
+            width: 200,
+            height: 200,
+            crop: true,
             noProfile: true
         }))
-        .pipe(gulp.dest('./public/img/logos'));
+        .pipe(image())
+        .pipe(dest('./public/img/logos'));
 
-    gulp.src('./assets/img/logos/*')
+    var logosRetina = src('./assets/img/logos/*.jpg')
+        .pipe(newer('./public/img/logos'))
         .pipe(imageResize({
-            width : 400,
-            height : 400,
-            crop : true,
+            width: 400,
+            height: 400,
+            crop: true,
             noProfile: true
         }))
-        .pipe(rename(function (path) { path.basename += "@2x"; }))
-        .pipe(gulp.dest('./public/img/logos'));
+        .pipe(image())
+        .pipe(rename(function (path) {
+            path.basename += "@2x";
+        }))
+        .pipe(dest('./public/img/logos'));
 
     //Profile resize
-    gulp.src('./assets/img/about/*')
+    var about = src('./assets/img/about/*.jpg')
+        .pipe(newer('./public/img/about'))
         .pipe(imageResize({
-            width : 300,
-            height : 300,
-            crop : true,
+            width: 300,
+            height: 300,
+            crop: true,
             noProfile: true,
             interlace: true
         }))
-        .pipe(gulp.dest('./public/img/about'));
+        .pipe(image())
+        .pipe(dest('./public/img/about'));
 
-    gulp.src('./assets/img/about/*')
+    var aboutRetina = src('./assets/img/about/*.jpg')
+        .pipe(newer('./public/img/about'))
         .pipe(imageResize({
-            width : 600,
-            height : 600,
-            crop : true,
+            width: 600,
+            height: 600,
+            crop: true,
             noProfile: true,
             interlace: true
         }))
-        .pipe(rename(function (path) { path.basename += "@2x"; }))
-        .pipe(gulp.dest('./public/img/about'));
+        .pipe(image())
+        .pipe(rename(function (path) {
+            path.basename += "@2x";
+        }))
+        .pipe(dest('./public/img/about'));
 
     //Header BG
-    gulp.src('./assets/img/header-bg.jpg')
+    var header = src('./assets/img/header-bg.jpg')
+        .pipe(newer('./public/img'))
         .pipe(imageResize({
-            width : 1900,
-            height : 1250,
+            width: 1900,
+            height: 1250,
             quality: 0.5,
-            crop : true,
+            crop: true,
             noProfile: true,
             interlace: true
         }))
-        .pipe(gulp.dest('./public/img'));
-});
-gulp.task('img', ['img:copy', 'img:resize']);
+        .pipe(image())
+        .pipe(dest('./public/img'));
 
-
-// Default task
-gulp.task('default', ['css', 'js', 'img', 'vendor']);
+    return merge(realisations, realisationsRetina, logos, logosRetina, about, aboutRetina, header);
+}
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'img'], function () {
-    gulp.watch('./assets/scss/*', ['css']);
-    gulp.watch('./assets/js/*.js', ['js']);
-    gulp.watch('./assets/img/*', ['img']);
-});
+function watchFiles() {
+    watch('./assets/scss/*', css);
+    watch('./assets/js/*.js', js);
+    watch('./assets/img/*', img);
+}
+
+// Define complex tasks
+const css = series(cssCompile, cssMinify);
+const js = series(jsConcat, jsMinify);
+
+const vendor = series(vendorGroup, vendorConcat);
+const build = series(vendor, parallel(css, js, img));
+const dev = series(parallel(css, js, img), watchFiles);
+
+exports.vendor = vendor;
+exports.css = css;
+exports.js = js;
+exports.img = img;
+exports.build = build;
+exports.dev = dev;
+exports.default = build;
