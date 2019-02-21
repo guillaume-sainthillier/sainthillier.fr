600608d69d631bfd2254570b4a4e526d4884960c
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2019-02-21 20:49:54 +0100

    SEO : set canonical url for homepage

--- public/index.php
+++ public/index.php
@@ -12,6 +12,7 @@ krsort($experiences); //Sort by year ASC
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link href="https://sainthillier.fr/" rel="canonical">
     <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
