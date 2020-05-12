10243b51760af0b97089a6ea530f6751d6b21108
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-05-12 12:19:19 +0200

    Added & ran prettier

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -1,15 +1,22 @@
 (function ($) {
-    "use strict"; // Start of use strict
+    'use strict'; // Start of use strict
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top - 54)
-                }, 1000, "easeInOutExpo");
+                $('html, body').animate(
+                    {
+                        scrollTop: target.offset().top - 54,
+                    },
+                    1000,
+                    'easeInOutExpo'
+                );
                 return false;
             }
         }
@@ -23,16 +30,16 @@
     // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
         target: '#mainNav',
-        offset: 56
+        offset: 56,
     });
 
     // Collapse Navbar
-    var navbar = $("#mainNav");
+    var navbar = $('#mainNav');
     var navbarCollapse = function () {
         if (navbar.offset().top > 100) {
-            navbar.addClass("navbar-shrink");
+            navbar.addClass('navbar-shrink');
         } else {
-            navbar.removeClass("navbar-shrink");
+            navbar.removeClass('navbar-shrink');
         }
     };
     // Collapse now if page is not at top
@@ -41,18 +48,20 @@
     $(window).scroll(navbarCollapse);
 
     // Hide navbar when modals trigger
-    $('.portfolio-modal').on('show.bs.modal', function (e) {
-        $('.navbar').addClass('d-none');
-    }).on('hidden.bs.modal', function (e) {
-        $('.navbar').removeClass('d-none');
-    });
+    $('.portfolio-modal')
+        .on('show.bs.modal', function (e) {
+            $('.navbar').addClass('d-none');
+        })
+        .on('hidden.bs.modal', function (e) {
+            $('.navbar').removeClass('d-none');
+        });
 
     $('.skills').each(function () {
         var datas = $(this).data('skills');
         var words = [];
 
         $.each(datas, function (name, value) {
-            words.push({text: name, weight: value});
+            words.push({ text: name, weight: value });
         });
 
         $(this).jQCloud(words);
