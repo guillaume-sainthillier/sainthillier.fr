31f31718f39ecee8b2bf3fef34b62d345750e321
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2021-06-22 17:24:06 +0200

    Added polyflills

--- assets/js/polyfills.js
+++ assets/js/polyfills.js
@@ -1,5 +1,7 @@
 import 'promise-polyfill/src/polyfill';
 import 'whatwg-fetch';
+import 'events-polyfill';
+import 'custom-event-polyfill';
 import 'element-matches-polyfill';
 import 'dom-node-polyfills';
 require('classlist-polyfill');
