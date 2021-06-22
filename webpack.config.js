42d0ace8ea52b1eac51c9ce580e93a3c53d2d982
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2021-06-22 13:27:05 +0200

    Play with module mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,128 +4,149 @@ const PurgecssPlugin = require('purgecss-webpack-plugin');
 const glob = require('glob-all');
 const path = require('path');
 
-const DEFAULT_BROWSER_TARGET = 'modern';
-const BROWSER_TARGET = process.env.BROWSERSLIST_ENV || DEFAULT_BROWSER_TARGET;
-const COPY_SUFFIX = BROWSER_TARGET === DEFAULT_BROWSER_TARGET ? '' : `_${BROWSER_TARGET}`;
+const makeConfig = (BROWSERSLIST_ENV) => {
+    const DEFAULT_BROWSER_TARGET = 'modern';
+    const BROWSER_TARGET = BROWSERSLIST_ENV || DEFAULT_BROWSER_TARGET;
+    const COPY_SUFFIX = BROWSER_TARGET === DEFAULT_BROWSER_TARGET ? '' : `_${BROWSER_TARGET}`;
 
-// Manually configure the runtime environment if not already configured yet by the "encore" command.
-// It's useful when you use tools that rely on webpack.config.js file.
-if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
-}
+    // Manually configure the runtime environment if not already configured yet by the "encore" command.
+    // It's useful when you use tools that rely on webpack.config.js file.
+    if (!Encore.isRuntimeEnvironmentConfigured()) {
+        Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+    }
 
-Encore
-    // directory where compiled assets will be stored
-    .setOutputPath(`static/build${COPY_SUFFIX}/`)
-    // public path used by the web server to access the output path
-    .setPublicPath(`/build${COPY_SUFFIX}/`)
-    // only needed for CDN's or sub-directory deploy
-    .setManifestKeyPrefix(`build${COPY_SUFFIX}/`)
+    Encore.reset();
+    Encore
+        // directory where compiled assets will be stored
+        .setOutputPath(`static/build${COPY_SUFFIX}/`)
+        // public path used by the web server to access the output path
+        .setPublicPath(`/build${COPY_SUFFIX}/`)
+        // only needed for CDN's or sub-directory deploy
+        .setManifestKeyPrefix(`build${COPY_SUFFIX}/`)
 
-    .copyFiles(
-        BROWSER_TARGET === DEFAULT_BROWSER_TARGET
-            ? [
-                  {
-                      from: './assets/images',
-                      to: Encore.isProduction() ? 'images/[path][name].[hash:8].[ext]' : 'images/[path][name].[ext]',
-                  },
-                  {
-                      from: './assets/svg',
-                      to: Encore.isProduction() ? 'svg/[path][name].[hash:8].[ext]' : 'svg/[path][name].[ext]',
-                  },
-                  {
-                      from: './assets/pdf',
-                      to: 'pdf/[path][name].[ext]',
-                  },
-              ]
-            : []
-    )
+        .copyFiles(
+            BROWSER_TARGET === DEFAULT_BROWSER_TARGET
+                ? [
+                      {
+                          from: './assets/images',
+                          to: Encore.isProduction()
+                              ? 'images/[path][name].[hash:8].[ext]'
+                              : 'images/[path][name].[ext]',
+                      },
+                      {
+                          from: './assets/svg',
+                          to: Encore.isProduction() ? 'svg/[path][name].[hash:8].[ext]' : 'svg/[path][name].[ext]',
+                      },
+                      {
+                          from: './assets/pdf',
+                          to: 'pdf/[path][name].[ext]',
+                      },
+                  ]
+                : []
+        )
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
-    .addEntry(BROWSER_TARGET, './assets/js/app.js')
+        /*
+         * ENTRY CONFIG
+         *
+         * Add 1 entry for each "page" of your app
+         * (including one that's included on every page - e.g. "app")
+         *
+         * Each entry will result in one JavaScript file (e.g. app.js)
+         * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+         */
+        .addEntry(BROWSER_TARGET, './assets/js/app.js')
 
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    .splitEntryChunks()
+        // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+        .splitEntryChunks()
 
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
-    .disableSingleRuntimeChunk()
+        // will require an extra script tag for runtime.js
+        // but, you probably want this, unless you're building a single-page app
+        .disableSingleRuntimeChunk()
 
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+        /*
+         * FEATURE CONFIG
+         *
+         * Enable & configure other features below. For a full
+         * list of features, see:
+         * https://symfony.com/doc/current/frontend.html#adding-more-features
+         */
+        .cleanupOutputBeforeBuild()
+        .enableBuildNotifications()
+        .enableSourceMaps(!Encore.isProduction())
+        // enables hashed filenames (e.g. app.abc123.css)
+        .enableVersioning(Encore.isProduction())
 
-    .configureBabel(() => {}, {
-        // node_modules is not processed through Babel by default
-        // but you can whitelist specific modules to process
-        includeNodeModules: ['bootstrap', 'lazysizes', 'lite-youtube-embed'],
-    })
-    // enables @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = 3;
-    })
-
-    // enables Sass/SCSS support
-    .enableSassLoader();
-
-if (Encore.isProduction()) {
-    Encore.addPlugin(
-        new PurgecssPlugin({
-            paths: glob.sync(
-                [
-                    path.join(__dirname, 'content/**/*.md'),
-                    path.join(__dirname, 'layouts/**/*.html'),
-                    path.join(__dirname, 'assets/js/*.js'),
-                    path.join(__dirname, 'node_modules/bootstrap/js/src/**/*.js'),
-                    path.join(__dirname, 'node_modules/lazysizes/lazysizes.js'),
-                    path.join(__dirname, 'node_modules/lite-youtube-embed/src/lite-yt-embed.js'),
-                ],
-                { nodir: true }
-            ),
-            safelist: {
-                standard: [/^weight-(\d+)$/],
-            },
+        .configureBabel(() => {}, {
+            // node_modules is not processed through Babel by default
+            // but you can whitelist specific modules to process
+            includeNodeModules: ['bootstrap', 'lazysizes', 'lite-youtube-embed'],
         })
-    );
-}
+        // enables @babel/preset-env polyfills
+        .configureBabelPresetEnv((config) => {
+            if (BROWSER_TARGET === 'modern') {
+                config.targets = { esmodules: true };
+            }
 
-Encore.addPlugin(
-    new FileManagerPlugin({
-        events: {
-            onEnd: {
-                copy: [
-                    {
-                        source: `./static/build${COPY_SUFFIX}/entrypoints.json`,
-                        destination: `./data/entrypoints${COPY_SUFFIX}.json`,
-                    },
-                    {
-                        source: `./static/build${COPY_SUFFIX}/manifest.json`,
-                        destination: `./data/manifest${COPY_SUFFIX}.json`,
-                    },
-                ],
+            config.browserslistEnv = BROWSER_TARGET;
+            config.useBuiltIns = 'usage';
+            config.corejs = 3;
+        })
+
+        // enables Sass/SCSS support
+        .enableSassLoader();
+
+    if (Encore.isProduction()) {
+        Encore.addPlugin(
+            new PurgecssPlugin({
+                paths: glob.sync(
+                    [
+                        path.join(__dirname, 'content/**/*.md'),
+                        path.join(__dirname, 'layouts/**/*.html'),
+                        path.join(__dirname, 'assets/js/*.js'),
+                        path.join(__dirname, 'node_modules/bootstrap/js/src/**/*.js'),
+                        path.join(__dirname, 'node_modules/lazysizes/lazysizes.js'),
+                        path.join(__dirname, 'node_modules/lite-youtube-embed/src/lite-yt-embed.js'),
+                    ],
+                    { nodir: true }
+                ),
+                safelist: {
+                    standard: [/^weight-(\d+)$/],
+                },
+            })
+        );
+    }
+
+    Encore.addPlugin(
+        new FileManagerPlugin({
+            events: {
+                onEnd: {
+                    copy: [
+                        {
+                            source: `./static/build${COPY_SUFFIX}/entrypoints.json`,
+                            destination: `./data/entrypoints${COPY_SUFFIX}.json`,
+                        },
+                        {
+                            source: `./static/build${COPY_SUFFIX}/manifest.json`,
+                            destination: `./data/manifest${COPY_SUFFIX}.json`,
+                        },
+                    ],
+                },
             },
-        },
-    }),
-    -11
-);
+        }),
+        -11
+    );
+
+    let config = Encore.getWebpackConfig();
+    config.name = BROWSERSLIST_ENV;
+
+    if (BROWSER_TARGET === 'modern') {
+        config.output.module = true;
+        config.output.libraryTarget = 'module';
+        config.experiments = config.experiments || {};
+        config.experiments.outputModule = true;
+    }
+    return config;
+};
 
 // uncomment if you use TypeScript
 //.enableTypeScriptLoader()
@@ -134,4 +155,5 @@ Encore.addPlugin(
 // requires WebpackEncoreBundle 1.4 or higher
 //.enableIntegrityHashes(Encore.isProduction())
 
-module.exports = Encore.getWebpackConfig();
+module.exports =
+    process.env.NODE_ENV === 'production' ? [makeConfig('modern'), makeConfig('legacy')] : [makeConfig('modern')];
